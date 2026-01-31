cc701a7ad0deb29f9c194b9ca40bc8e52bdb2313
server1259000-ship-it <server1259000@gmail.com>
2026-02-01 01:27:34 +0500

    Update get-dresses.js

--- api/get-dresses.js
+++ api/get-dresses.js
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
 
 export default async function handler(req, res) {
   try {
-    // Ye line database se saara updated data (Price, Link, etc.) nikaal kar laati hai
+    // Ye line database se data nikaalti hai
     const { rows } = await sql`SELECT * FROM dresses ORDER BY id DESC;`;
     return res.status(200).json(rows);
   } catch (error) {
