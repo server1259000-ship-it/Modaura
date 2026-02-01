efc6ae4ce584571466c82b3e859219a23d8b28d9
server1259000-ship-it <server1259000@gmail.com>
2026-02-01 18:35:29 +0500

    Update save.js

--- api/save.js
+++ api/save.js
@@ -1,14 +1,16 @@
-import { sql } from '@vercel/postgres';
+import { neon } from '@neondatabase/serverless';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    const sql = neon(process.env.DATABASE_URL);
     const { name, desc, url, price, affiliate_link, category } = req.body;
+    
     try {
-      await sql`INSERT INTO dresses (name, description, url, price, affiliate_link, category) 
-                VALUES (${name}, ${desc}, ${url}, ${price}, ${affiliate_link}, ${category});`;
-      return res.status(200).json({ message: 'Success' });
+      await sql('INSERT INTO dresses (name, description, url, price, affiliate_link, category) VALUES ($1, $2, $3, $4, $5, $6)', 
+      [name, desc, url, price, affiliate_link, category]);
+      res.status(200).json({ message: 'Success' });
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   }
 }
