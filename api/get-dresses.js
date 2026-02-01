71d72a7ef1ab32e055b0896d77ad157635bbaae7
server1259000-ship-it <server1259000@gmail.com>
2026-02-02 00:06:13 +0500

    Update get-dresses.js

--- api/get-dresses.js
+++ api/get-dresses.js
@@ -1,11 +1,12 @@
-import { neon } from '@neondatabase/serverless';
+import { Redis } from '@upstash/redis'
+
+const redis = Redis.fromEnv()
 
 export default async function handler(req, res) {
-  const sql = neon(process.env.DATABASE_URL);
   try {
-    const posts = await sql('SELECT * FROM dresses ORDER BY id DESC');
-    res.status(200).json(posts);
+    const dresses = await redis.get('dresses');
+    return res.status(200).json(dresses || []);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 }
