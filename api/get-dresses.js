1f89e5c2fc56fe12c7dbc51148480762ae868594
server1259000-ship-it <server1259000@gmail.com>
2026-02-16 19:59:18 +0500

    Update get-dresses.js

--- api/get-dresses.js
+++ api/get-dresses.js
@@ -7,9 +7,12 @@ const redis = new Redis({
 
 export default async function handler(req, res) {
   try {
+    // Redis se sari dresses uthana
     const dresses = await redis.lrange('dresses', 0, -1);
-    // Upstash data ko sahi format mein bhej raha hai
+    
+    // Har item ko check karna ke wo sahi format mein hai ya nahi
     const data = dresses.map(item => typeof item === 'string' ? JSON.parse(item) : item);
+    
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
